f8cde46c3fdf0579cdace2b81d54fbb7dbc51922
ellner <spe2@cornell.edu>
2026-01-15 17:56:58 -0500

    conditional lifespan

--- stagecoach/conditional_lifespan_funs.R
+++ stagecoach/conditional_lifespan_funs.R
@@ -10,10 +10,29 @@
 ######################### NOTATION CONVENTIONS ############################
 ##      luckieR M = StageCoach P (state transitions) 
 ##      luckieR F = StageCoach B (sexual births) + F (clonal births, fission) 
-###########################################################################          
+########################################################################### 
 
-conditional_mean_lifeSpan <- function(M){
-  
+
+########################################################################### 
+## Compute mean life expectancy given current state, for all states,     ##
+##		given by row sums of the fundamental matrix                      ##
+##  Arguments:                                                           ##
+##  		M: state transition matrix M                                 ##
+##  Return Value/s:                                                      ##
+##        Mean lifespan expectancy for each state i  					 ##
+########################################################################### 
+life_expectancy = function(M){ 
+  matDim = nrow(M); 
+  N <- try(solve(diag(matDim) - M), silent = TRUE)
+  if (inherits(N, "try-error")) {
+        LE = rep(NA,matDim)
+		} else {
+        LE <- rep(1, matDim) %*% N
+  }
+  return(LE)
+}  
+
+
 #########################################################################
 ##  Function to determine mean of lifespan conditional on individual   ##
 ##      reaching state i before death.                                 ##
@@ -23,39 +42,27 @@ conditional_mean_lifeSpan <- function(M){
 ##  Return Value/s:                                                    ##
 ##        Conditional mean lifespan for each state i                   ##
 #########################################################################
-
-  P = M; ## go to StageCoach notation  
-  if(!is.matrix(P)) {stop("This is not a matrix")}
-  if(!is.square.matrix(P)) {stop("This is not a square matrix")}
-  if(!is.numeric(P)) {stop("This is not numeric")}
+conditional_mean_lifeSpan <- function(M){
+  if(!is.matrix(M)) {stop("This is not a matrix")}
+  if(!is.square.matrix(M)) {stop("This is not a square matrix")}
+  if(!is.numeric(M)) {stop("This is not numeric")}
   
-
-  ## Compute life expectancy given current state 
-  matDim = nrow(P); 
-  N <- try(solve(diag(matDim) - P), silent = TRUE)
-  if (inherits(N, "try-error")) {
-        LE = rep(NA,matDim)
-		} else {
-        LE <- rep(1, matDim) %*% N
-    }
-
-################ STEVE STOPPED HERE   
-  
-  LE <- life_expectancy(P)
+  LE <- life_expectancy(M)
+ 
  # must add each stage number to the correct vector
-  MT <- meantime(P)
-  results <- matrix(0,nrow = nrow(P),ncol = ncol(P))
-  for (i in 1:dim(P)[1]) {
-   for (j in 1:dim(P)[2]) {
-     results[i,j] <- MT[i,j] + LE[i] + 1
-     if(i < j){
-       results[i,j] <- -1
-     }
-     else{
-       results[i,j] <- MT[i,j] + LE[i] + 1
-   }
-     }
- }
-  results[results < 0] < NA
+  MT <- meantime(M)
+  results <- matrix(0,nrow = nrow(M),ncol = ncol(M))
+  for (i in 1:nrow(M)) {
+    for (j in 1:nrow(M) {
+      results[i,j]  = MT[i,j] + LE[i] + 1
+      if(i < j){
+         results[i,j] = -1
+       } else{
+         results[i,j] = MT[i,j] + LE[i] + 1
+       }
+    }
+  }
+  results[results == 0] <- NA
   return(results)
 }
+
