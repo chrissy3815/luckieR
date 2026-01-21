2c7e95b565bfe71a58f6730dcb68b74f02e7df2a
ellner <spe2@cornell.edu>
2026-01-20 19:17:11 -0500

    working on stagecoach funs

--- stagecoach/conditional_lifespan_funs.R
+++ stagecoach/conditional_lifespan_funs.R
@@ -1,16 +1,41 @@
-###########################################################################
-## R translation of formulas for mean/sd of conditional lifespan,from
+#############################################################################
+## R translations of formulas for mean/var of conditional lifespan, and 
+## mean/var of conditional times to reach state i from state j, from 
 ##     Cochran, M. E. and S. Ellner.  1992.  Simple methods for 
 ##     calculating age-based life history parameters for stage-
 ##     structured populations. Ecological Monographs 62:345-364.
 ##
 ## Original coding by Erin Souder, merge into luckieR by Steve Ellner 
-###########################################################################
+## NOTE: 																   
+##     CE92 used the convention that all times (lifetime, transition time, etc.) 
+##     are the largest possible value consistent with the census data. 
+##     But that convention gives annuals a lifespan of 2 years, which was        
+##     generally considered to be a bad idea, so we abandon it here. 
 
-######################### NOTATION CONVENTIONS ############################
+##     Your lifespan is the number of times that you were alive at a census 
+##     time (rather than that number plus one). 
+## 
+##     For transition times, a state-j newborn *could have* lived up to one 
+## 	   step prior to being censused in state j, so the CE92 formula gives 1 as 
+##	   the conditional mean time to reach state j starting from state j. 
+##     The code here gives instead 0: if you are first censuses in state j,  
+## 	   we assume that it took you 'no time' to get there.
+##############################################################################                                          
+                  
+##############################################################################
+
+######################### NOTATION CONVENTIONS  ##############################
 ##      luckieR M = StageCoach P (state transitions) 
 ##      luckieR F = StageCoach B (sexual births) + F (clonal births, fission) 
-########################################################################### 
+############################################################################## 
+
+### The defined functions are:
+##			mean_lifespan, var_lifespan
+##			Di_mat (a utility function)
+## 			mean_conditional_time
+##			mean_conditional_lifespan
+##			var_conditional_time
+##			var_conditional_lifespan (NOT FINISHED) 
 
 #### For testing 
 library(readxl); 
@@ -19,22 +44,40 @@ M = as.matrix(read_xlsx("Caswell_P.xlsx"));
 F = as.matrix(read_xlsx("Caswell_B.xlsx")); 
 
 
-M1 = matrix(c(0, 0,  0,  0,
-              1, 0,  0,  0,
-			  0, 0.5, 0, 0,
-			  0, 0.5, 1, 0), 4,4,byrow=TRUE) 
+M1 = matrix(c(0, 0,    0,  0,
+              1, 0,    0,  0,
+			  0, 0.5,  0,  0,
+			  0, 0.5,  1,  0), 4,4,byrow=TRUE) 
 N1 = solve(diag(4)-M1); colSums(N1); 			  
-## 3.5 2.5 2.0 1.0   
+## 3.5 2.5 2.0 1.0  
 
+M2 = matrix(c(0, 0,    0,  0,
+              1, 0,    0,  0,
+			  0, 0.25, 0,  0,
+			  0, 0.25, 1,  0), 4,4,byrow=TRUE) 
+N2 = solve(diag(4)-M2); colSums(N2); 			  
+## 2.75 1.75 2.00 1.00
+ 
 ########################################################################### 
-## Compute mean life expectancy given current state, for all states,     ##
-##		given by row sums of the fundamental matrix                      ##
-##  Arguments:                                                           ##
-##  		M: state transition matrix M                                 ##
-##  Return Value/s:                                                      ##
-##        Mean lifespan expectancy for each state i  					 ##
+## Compute mean remaining lifespan given current state, for each state,       
+##		given by row sums of the fundamental matrix. "Remaining lifespan"
+##      includes the current year --- it is the number of years when the
+##		individual is alive to be censused, including the current year and
+##      all subsequent years, but not prior years (if there were any). 
+##  Arguments:                                                            
+##  		M: state transition matrix M                                  
+##  Return Value/s:                                                       
+##        Mean lifespan expectancy for each state i  					  
+## 
+##  Similar to life_expect_mean in Rage, but returns a vector of values
+##  for each state, rather than one value based on a mixing distribution
+##  across initial states 
 ########################################################################### 
-life_expectancy = function(M){ 
+mean_lifespan = function(M){ 
+  if(!is.matrix(M)) {stop("This is not a matrix")}
+  if(nrow(M)!=ncol(M)) {stop("This is not a square matrix")}
+  if(!is.numeric(M)) {stop("This is not numeric")}
+
   matDim = nrow(M); 
   N <- try(solve(diag(matDim) - M), silent = TRUE)
   if (inherits(N, "try-error")) {
@@ -44,17 +87,48 @@ life_expectancy = function(M){
   }
   return(LE)
 }  
+ 
+## mean_lifespan(M1);
+## [1] 3.5 2.5 2.0 1.0
+## Give the same results as life_expect_mean(M1,start=j) for j = 1,2,3,4
+
+########################################################################### 
+## Compute variance of remaining lifespan given current state. See 
+##      mean_lifespan for the definition of "remaining lifespan". 
+## Equation 5 in Cochran and Ellner 1992, which is equivalent 
+##      to eqn. 5.12 in Caswell 2001 (proof: multiply each formula
+##      for B on the right by N%*%N, and the result for both is (I+M).
+##  Arguments:                                                            
+##  		M: state transition matrix M                                  
+##  Return Value/s:                                                       
+##        Variance of remaining lifespan given current state i  					  
+## 
+##  Similar to life_expect_var in Rage, but returns a vector of values
+##  for each state, rather than one value based on a mixing distribution
+##  across initial states.  
+########################################################################### 
+var_lifespan = function(M){ 
+  if(!is.matrix(M)) {stop("This is not a matrix")}
+  if(nrow(M)!=ncol(M)) {stop("This is not a square matrix")}
+  if(!is.numeric(M)) {stop("This is not numeric")}
+  
+  eye = diag(nrow(M)); 
+  N = solve(eye-M); 
+  B = (eye + M)%*%N%*%N;
+  results = colSums(B) - mean_lifespan(M)^2; 
+  return(results)
+}  
 
 ########################################################################
-##  Function to calculate modified transition matrix D_i for state i, ##
-##      where individuals entering state i stay there for one time    ##
-##      step and then die. This is a utility for meantime() and       ##
-##      generally not of value to users.                              ##
-##  Equation 8 in Cochran and Ellner 1992                             ##
-##  Argument/s:                                                       ##
-##     M: state transition matrix M                                   ##
-##  Return Value/s:                                                   ##
-##     Array of D of transition matrices, such that D[,,i] is D_i     ##
+##  Function to calculate modified transition matrices D_i for each    
+##      state i, where individuals entering state i stay there one     
+##      time step and then die. This function is used by meantime()    
+##      and would not be accessed directly by users.                   
+##  Equation 8 in Cochran and Ellner 1992                              
+##  Argument/s:                                                        
+##     M: state transition matrix M                                    
+##  Return Value/s:                                                    
+##     Array of D of transition matrices, such that D[,,i] is D_i      
 ########################################################################
 Di_mat <- function(M){
   if(!is.matrix(M)) {stop("This is not a matrix")}
@@ -70,23 +144,14 @@ Di_mat <- function(M){
 }
 
 ############################################################################
-##  Function to compute the average time to reach state i from state j,   ##
-##         conditional on that happening                                  ## 
-##  Equation 9 in Cochran and Ellner 1992                                 ##
-##  Argument/s:                                                           ##
-##     M: state transition matrix M                                       ##
-##  Return Value/s:                                                       ##
-##     Matrix, (i,j) entry = conditional mean time to reach i from j,     ##
-##       with NA if the transition is not possible.                       ##
-##  NOTE: 																  ##
-##     We subtract 1 from the CE92 formula. In CE92, all times are the    ##
-##	   largest possible value consistent with the data, in particular: a  ##
-##	   state-j newborn *could have* taken one time step to get where it   ##
-##     now is, so the CE92 formula gives 1 on the diagonal of the result. ##
-##     Here we put zeros on the diagonal: if first seen in state j, it    ##
-##     too you 'no time' to get there.                                    ##
-##     The CE92 convention gives annuals a lifespan of 2 years. That      ##
-##     was generally considered to be a bad idea.                         ##
+##  Function to compute the average time to reach state i from state j,    
+##         conditional on that happening                                    
+##  Equation 9 in Cochran and Ellner 1992                                  
+##  Argument/s:                                                            
+##     M: state transition matrix M                                        
+##  Return Value/s:                                                        
+##     Matrix, (i,j) entry = conditional mean time to reach i from j,      
+##       with NA if the transition from j to i is not possible.                        
 ############################################################################
 mean_conditional_time <- function(M){
   if(!is.matrix(M)) {stop("This is not a matrix")}
@@ -109,81 +174,24 @@ mean_conditional_time <- function(M){
   return(results)
 }
 
-SD_conditional_time <- function(M){
-    
-  ######################################################################################
-  ##  Function to determine the SD of the time to reach stage i from stage j          ##
-  ##     conditional on that happening.                                               ##  
-  ##  Equation 10 in Cochran and Ellner 1992                                          ##
-  ##  Argument/s:                                                                     ##
-  ##  P: Survival matrix P                                                            ##
-  ##  Return Value/s:                                                                 ##
-  ##  The standard deviation for the time to reach stage i from stage j,              ##
-  ##           conditional on that happening 
-  ##  Author/s:                                                                       ##
-  ##  Dr. Stephen Ellner                                                              ##
-  ##  Dr. Simone Blomberg                                                             ##
-  ##  Erin Souder                                                                     ##
-  ##  Date: 03/01/2023                                                                ##
-  ######################################################################################
-  
-  if(!is.matrix(P)) {stop("This is not a matrix")} 
-  if(!is.square.matrix(P)) {stop("This is not a square matrix")}
-  if(!is.numeric(P)) {stop("This is not numeric")}
-  
-  #OUTPUT DOESN'T MATCH STAGECOACH
-  D <- Di_mat(P)
-  m0 <- meantime(P)
-  m1 <- m0 + 1
-  # m1 used to match equations from paper
-  results <- matrix(0, nrow = nrow(P), ncol= ncol(P))
-  # Make new matrix 
-  
-  for (i in 1:dim(results)[1]) {
-    Imat <- diag(x=1, dim(D[,,i])[1])
-    num <- (Imat + D[,,i]) %*% solve((Imat - D[,,i]) %*% (Imat - D[,,i]) %*% (Imat - D[,,i])) 
-    den <- solve(Imat - D[,,i]) 
-    results[i,] <- (num[i,]/den[i,])
-    results[i,] <- sqrt(results[i,] - (m1[i,]) ^ 2)
-  }
-  results[is.nan(results)] <- NA
-  results[m0 < 0] <- NA
-  # Transition not defined for this so it's NA
- return(results)
-  
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 #########################################################################
-##  Function to determine mean of lifespan conditional on reaching     ##
-##      state i before death, for individuals born in state j.         ##
-##  Equation 6 in Cochran and Ellner 1992                              ##
-##  Arguments:                                                         ##
-##  		M: state transition matrix M                               ##
-##  Return Value/s:                                                    ##
-##        Conditional mean lifespan for each state i                   ##
+##  Function to determine mean of lifespan conditional on reaching      
+##      state i before death, for individuals born in state j.          
+##  Equation 6 in Cochran and Ellner 1992                               
+##  Arguments:                                                          
+##  	M: state transition matrix M                                
+##  Return Value/s:   
+##     Matrix, (i,j) entry = mean lifespan conditional on reaching
+##       state i before death, for an individual born in state j, 
+##       with NA if the transition from j to i is not possible.                 
 #########################################################################
-conditional_mean_lifeSpan <- function(M){
+mean_conditional_lifespan <- function(M){
   if(!is.matrix(M)) {stop("This is not a matrix")}
   if(nrow(M)!=ncol(M)) {stop("This is not a square matrix")}
   if(!is.numeric(M)) {stop("This is not numeric")}
   
-  LE <- life_expectancy(M)
-  MT <- meantime(M)
+  LE <- mean_lifespan(M)
+  MT <- mean_conditional_time(M)
   
   results <- matrix(0,nrow = nrow(M),ncol = ncol(M))
   for (i in 1:nrow(M)) {
@@ -191,4 +199,72 @@ conditional_mean_lifeSpan <- function(M){
   }
   return(results)
 }  
+
+######################################################################################
+##  Function to determine the variance of the time to reach stage i from stage j           
+##     conditional on that happening.                                                  
+##  Equation 10 in Cochran and Ellner 1992                                           
+##  Argument/s:                                                                      
+##  	M: state transition matrix                                                             
+##  Return Value/s:                                                                  
+##      Matrix, (i,j) entry = variance of the time to reach i from j, conditional     
+##       on that happening, with NA if the transition is not possible.  
+##
+##   Note: the difference in time conventions (shorter here by 1 than in CE92)
+##     does not affect the variance, so here we use the CE92 convention and formula.
+######################################################################################
+var_conditional_time <- function(M){
+  if(!is.matrix(M)) {stop("This is not a matrix")}
+  if(nrow(M)!=ncol(M)) {stop("This is not a square matrix")}
+  if(!is.numeric(M)) {stop("This is not numeric")}
+  
+  D  = Di_mat(M)
+  tau = mean_conditional_time(M) + 1 ## CE92 convention 
+  
+  results <- matrix(0, nrow(M), ncol(M))
+  Imat <- diag(nrow(M))
+  
+  for (i in 1:nrow(M)) {
+    Ni = solve(Imat - D[,,i])
+    num <- (Imat + D[,,i]) %*% Ni %*% Ni %*% Ni 
+    results[i,] <- (num[i,]/Ni[i,]) - tau[i,]^2; 
+  }
+  results[is.nan(results)] <- NA
+  return(results)
+  
+}
+
+  
+#########################################################################
+##  Function to calculate variance of lifespan conditional on reaching      
+##      state i before death, for individuals born in state j.          
+##  Equation 7 in Cochran and Ellner 1992                               
+##  Arguments:                                                          
+##  	M: state transition matrix M                                
+##  Return Value/s:   
+##     Matrix, (i,j) entry = variance of lifespan conditional on reaching
+##       state i before death, for an individual born in state j, 
+##       with NA if the transition from j to i is not possible.                 
+#########################################################################
+var_conditional_lifespan  = function(M){
+  if(!is.matrix(M)) {stop("This is not a matrix")}
+  if(nrow(M)!=ncol(M)) {stop("This is not a square matrix")}
+  if(!is.numeric(M)) {stop("This is not numeric")}
+  
+  Omega <- var_conditional_time(M)
+  
+  l <- life_expectancy_SD(P)
+  results <- matrix(0,nrow = nrow(P), ncol = ncol(P))
+  for (i in 1:nrow(P)) {
+    
+      results[i,] <- l[i] ^ 2 + m[i,] ^ 2  
+    }
+    
+  return(sqrt(results))
+  
+}
+
+
+
+
 
